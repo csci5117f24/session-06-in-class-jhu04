835665ad106f2db759ae9e033f6d1cb6e1d499ba
Jeffrey Hu <jeffreyhu8@gmail.com>
2024-09-25 17:38:06 -0500

    Added database querying

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+import os
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    with psycopg2.connect(os.environ['DATABASE_URL']) as conn:
+        with conn.cursor() as cur:
+            cur.execute("select * from tmp")
+            return render_template('hello.html', name=cur.fetchone()[0])
